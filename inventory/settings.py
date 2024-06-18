84884759c6769b5de54e212f4ca39d472c058792
raonarvokshi <raonar.vokshi@gmail.com>
2024-06-18 19:07:33 +0200

    Fixed some responsive bugs

--- inventory/settings.py
+++ inventory/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2f262a4cc7693b2778c48d9134617936e69d5bc53f75b855877cdbf3bc9095e5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
